d766c7ff4aa5910087705c445e683b214c577c93
Isaac Bedard <brokenLogician@gmail.com>
2017-11-16 21:35:27 -0500

    Create input component
    Behold!! My glorious input component!!!
    It's not currently being used anywere, and it looks very, very drab. It doesn't even work yet.
    But it's mine.

---
+++ khe-ui-template/src/components/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="login">
+    <label for="login-username">Username</label>
+    <input id="login-username" type="text" />
+
+    <label for="login-password">Password</label>
+    <input id="login-password" type="password" />
+
+    <input id="login-button" type="button" value="Login"/>
+
+    <input id="login-create-account" type="button" value="Create an Account" />
+
+    <a href="#">Forgot Password</a>
+  </div>
+</template>
+
+<style scoped>
+ 
+</style>
+
+<script>
+
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+    };
+  },
+};
+</script>
