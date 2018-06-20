63769805610405ea1d0c27c383479edd18afbde2
Ben Holland <bhollan5@kent.edu>
2018-06-20 16:28:27 -0400

    Fixed menu bar and made it pretty

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <template>
   <div id="app">
-     <div id="banner"
-         v-bind:style="{ minHeight: mobileMenu + 'px' }">
+     <div id="banner">
       <div id="bannerL" class="bannerContainer">
 <!--        <img id="bannerLogo" 
              src="./assets/blackLogo.png">-->
-        <router-link to="/" class="routerLink">
-        <p class="bannerText" id="kheTitle"
+        <p class="banner-link" id="kheTitle"
            @click="scrollTo('/', '#mainContainer')">KENT HACK ENOUGH</p>
-        </router-link>
         
         <div id="hamburgMenu">
           <img id="hamburgIcon" src="../static/Hamburger_icon.svg.png"
@@ -19,30 +16,40 @@
       
       
       
-      <div id="bannerR" class="bannerContainer">
+      <div id="bannerR" class="bannerContainer"
+           :class="{'hidden': expandMenu}">
         
-        <router-link to="/" class="routerLink">
-          <p class="bannerText bannerLink"
-            @click="scrollTo('/', '#info')">About</p>
-        </router-link>
         
-        <router-link to="/" class="routerLink">
-          <p class="bannerText bannerLink"
-            @click="scrollTo('/', '#faq')">FAQ</p>
-        </router-link>
-        <p class="bannerText bannerLink">Sponsors</p>
-        <p class="bannerText bannerLink"
+        <p class="banner-link pink"
+            @click="scrollTo('/', '#info')">
+          About
+        </p>
+        
+        <p class="banner-link green"
+            @click="scrollTo('/', '#faq')">
+          FAQ
+        </p>
+        
+        <p class="banner-link blue">
+          Sponsors
+        </p>
+        
+        <p class="banner-link yellow"
            @click="navTo('/contact')">Contact</p>
-        <p class="bannerText bannerLink"
+        
+        <p class="banner-link pink"
            @click="scrollTo('/', '#map')">Map</p>
-        <p class="bannerText bannerLink" @click="dispRegister()"
+        
+        <p class="banner-link green" @click="dispRegister()"
            v-if="user._id == ''">
           Register/Login
         </p>
-        <p class="bannerText bannerLink" @click='logout()'
+        
+        <p class="banner-link blue" @click='logout()'
            v-if="user._id != ''">
           Log out
         </p>
+        
         <div class="padding" style="width: 50px"></div>
       </div>
       
@@ -82,7 +89,7 @@ export default {
       
       scrollToEl: '',
       
-      mobileMenu: 50,
+      expandMenu: false,
       
       user: {
         _id: '',
@@ -124,11 +131,11 @@ export default {
     dispLogin: function() {
       // Normally we could do a one line function like this inside an @click attribute,
       // but this makes it easier to access from it's children
-      this.mobileMenu = 50;
+      this.expandMenu = false;
       this.showLogin = !this.showLogin;
     },
     dispRegister: function() {
-      this.mobileMenu = 50;
+      this.expandMenu = false;
       this.showRegister = !this.showRegister;
     },
     switchLoginRegister: function() {
@@ -141,15 +148,11 @@ export default {
     },
 
     togMenu: function() {
-      if (this.mobileMenu === 50) {
-        this.mobileMenu = 375;
-      } else {
-        this.mobileMenu = 50;
-      }
+      this.expandMenu = !this.expandMenu;
     },
     
     scrollTo: function(page, el) {
-      this.mobileMenu = 50;
+      this.expandMenu = false;
       if (this.$route.path !== page) {
         this.$router.push(page);
         this.scrollToEl = el;
@@ -158,7 +161,7 @@ export default {
       }
     },
     navTo: function(page) {
-      this.mobileMenu = 50;
+      this.expandMenu = false;
       this.$router.push(page);
     }
   }
@@ -177,11 +180,7 @@ export default {
     justify-content: space-between;
     color: white;
   }
-  .routerLink {
-    color: white;
-    margin-right: 20px;
-    margin-left: 20px;
-  }
+  
   .bannerContainer {
     display: flex;
   }
@@ -200,11 +199,14 @@ export default {
     --orange: #ff936b;
     --pink: #ff74b9;
     --green: #82fa6b;
+    --blue: #64CDF6;
     --yellow: #effc6a;
     --moon: #fcfede;
     --light-brown: #af6c56;
     --dark-brown: #663c2e;
     --blue: #66daff;
+    
+    --breakpoint: 900px;
   }
 
   html, body {
@@ -227,4 +229,122 @@ export default {
     }
   }
   
+</style>
+
+<style scoped>
+  
+  #hamburgMenu {
+    display: none;
+  }
+  
+  
+  @media only screen and (min-width: 850px) {
+/*    NOTE for anyone trying to learn: this would be 1000% easier with sass!*/
+    .pink {
+      border-bottom: solid 3px var(--pink);
+    }
+    .green {
+      border-bottom: solid 3px var(--green);
+    }
+    .blue {
+      border-bottom: solid 3px var(--blue);
+    }
+    .yellow {
+      border-bottom: solid 3px var(--yellow);
+    }
+    .pink:hover {
+      border-bottom: solid 5px var(--pink);
+      padding-bottom: 8px;
+    }
+    .green:hover {
+      border-bottom: solid 5px var(--green);
+      padding-bottom: 8px;
+    }
+    .blue:hover {
+      border-bottom: solid 5px var(--blue);
+      padding-bottom: 8px;
+    }
+    .yellow:hover {
+      border-bottom: solid 5px var(--yellow);
+      padding-bottom: 8px;
+    }
+  }
+  
+/*  Mobile style:  */
+  @media only screen and (max-width: 850px) {
+    #bannerL {
+      display: flex;
+      justify-content: space-between
+    }
+    
+    #bannerR {
+      flex-direction: column;
+    }
+    #bannerR:not(.hidden) {
+      display: none;
+    }
+    
+    #banner {
+      flex-direction: column;
+    }
+    #hamburgMenu {
+      display: block; 
+      cursor: pointer;
+    }
+    #hamburgIcon {
+      filter: invert(100%);
+      width: 40px;
+      height: 40px;
+      padding: 5px;
+    }
+    #bannerLMobile {
+      display: block;
+    }
+    .bannerLink {
+      margin-bottom: 0px;
+      padding-bottom: 10px;
+      border: solid black 1px;
+    }
+    
+    .pink {
+      border-left: solid 3px var(--pink);
+    }
+    .green {
+      border-left: solid 3px var(--green);
+    }
+    .blue {
+      border-left: solid 3px var(--blue);
+    }
+    .yellow {
+      border-left: solid 3px var(--yellow);
+    }
+    .pink:hover {
+      border-left: solid 8px var(--pink);
+      padding-left: 15px;
+    }
+    .green:hover {
+      border-left: solid 8px var(--green);
+      padding-left: 15px;
+    }
+    .blue:hover {
+      border-left: solid 8px var(--blue);
+      padding-left: 15px;
+    }
+    .yellow:hover {
+      border-left: solid 8px var(--yellow);
+      padding-left: 15px;
+    }
+  }
+  
+  .banner-link {
+    color: white;
+    padding: 15px 20px 10px 20px;
+    margin: 0px;
+    height: 100%;
+    cursor: pointer;
+    transition-duration: .2s;
+  }
+  .banner-link:hover {
+    background: #555;
+  }
 </style>
