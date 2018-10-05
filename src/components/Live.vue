8bc4a84c2441cf6f9f560c8836378e2f2ef29a75
Ben Holland <bhollan5@kent.edu>
2018-10-05 18:47:53 -0400

    Prototyped the layout for /live

--- src/components/Live.vue
+++ src/components/Live.vue
@@ -1,11 +1,49 @@
 <template>
-    <b>Hello Live</b>
+<div id="live">
+  <div id="updates">
+
+  </div>
+  <div id="schedule">
+
+  </div>
+  <div id="leaderboard">
+
+  </div>
+</div>
 </template>
 
 <script>
 
 </script>
 
-<style>
+<style scoped>
+#live {
+  position: absolute;
+  left: 0px;
+  width: calc(100% - 0px);
+  height: calc(100% - 50px);
+  background: #484B5B;
+  display: grid;
+  grid-template-columns: 33% 40% 25%;
+  grid-template-rows: 50% 35% 15%;
+  grid-column-gap: 1%;
+  padding: 20px;
+}
 
+#updates {
+  background: #D6BDD6;
+  grid-column: 1 / 1;
+  grid-row: 1 / 4;
+}
+#schedule {
+  background: #E0F2D3;
+  grid-column: 2 / 2;
+  grid-row: 1 / 4;
+}
+
+#leaderboard {
+  background: #FEE0CC;
+  grid-column: 3 / 3;
+  grid-row: 1 / 1;
+}
 </style>
