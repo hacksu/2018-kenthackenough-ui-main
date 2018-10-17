ac8c8f578e20fbd537520dea952f356ecf716443
Brojowski <alex.gajowski@gmail.com>
2018-10-16 20:04:10 -0400

    add gamifyV1 scoreboard

---
+++ src/components/Scoreboard.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>Scoreboard
+        <table>
+            <tr>
+                <th>User</th>
+                <th>Points</th>
+            </tr>
+            <tr v-for="score in scores" :key="score._id">
+                <td>{{ score.email }}</td>
+                <td>{{ score.points }}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    main: 'Main',
+    data() {
+        return {
+            scores: []
+        }
+    },
+    mounted() {
+        let self = this;
+        
+        this.$parent.wrapper.gamifyV1.scoreboard()
+        .then((scores) => {
+            self.scores = scores;
+        })
+        .catch((err) => {
+            throw err;
+        });
+    }
+}
+</script>
+
+<style>
+
+</style>
+
+
